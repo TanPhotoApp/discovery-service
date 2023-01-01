83d08cc87ddebf5ee2d588d68b997b6196c82cbc
tanlh <huutanctct@gmail.com>
2023-01-01 13:35:21 +0700

    Rename service

--- src/main/java/com/photoapp/discovery/DiscoveryApplication.java
+++ src/main/java/com/photoapp/discovery/DiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class PhotoAppDiscoveryApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PhotoAppDiscoveryApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 
 }
